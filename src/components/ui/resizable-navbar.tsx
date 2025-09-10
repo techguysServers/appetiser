050614e94ee9c310c5f053cbad747624b7f68e01
Miunn <remi@remicaulier.fr>
2025-09-10 13:29:18 -0400

    feat(logo): add logo files and set it up in navbar

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -9,6 +9,8 @@ import {
 } from "motion/react";
 
 import React, { useRef, useState } from "react";
+import AppLogo from "../common/logo";
+import Image from "next/image";
 
 interface NavbarProps {
   children: React.ReactNode;
@@ -74,9 +76,9 @@ export const Navbar = ({ children, className }: NavbarProps) => {
         React.isValidElement(child)
           ? React.cloneElement(
               child as React.ReactElement<{ visible?: boolean }>,
-              { visible }
+              { visible },
             )
-          : child
+          : child,
       )}
     </motion.div>
   );
@@ -104,7 +106,7 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
       className={cn(
         "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-2 lg:flex dark:bg-transparent",
         visible && "bg-white/80 dark:bg-neutral-950/80",
-        className
+        className,
       )}
     >
       {children}
@@ -120,7 +122,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
       onMouseLeave={() => setHovered(null)}
       className={cn(
         "absolute inset-0 hidden flex-1 flex-row items-center justify-center space-x-2 text-sm font-medium text-zinc-600 transition duration-200 hover:text-zinc-800 lg:flex lg:space-x-2",
-        className
+        className,
       )}
     >
       {items.map((item, idx) => (
@@ -166,7 +168,7 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
       className={cn(
         "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent px-0 py-2 lg:hidden",
         visible && "bg-white/80 dark:bg-neutral-950/80",
-        className
+        className,
       )}
     >
       {children}
@@ -182,7 +184,7 @@ export const MobileNavHeader = ({
     <div
       className={cn(
         "flex w-full flex-row items-center justify-between",
-        className
+        className,
       )}
     >
       {children}
@@ -205,7 +207,7 @@ export const MobileNavMenu = ({
           exit={{ opacity: 0 }}
           className={cn(
             "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950",
-            className
+            className,
           )}
         >
           {children}
@@ -235,7 +237,7 @@ export const NavbarLogo = () => {
       href="#"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
-      <img
+      {/*<img
         src="https://assets.aceternity.com/logo-dark.png"
         alt="logo"
         width={30}
@@ -243,7 +245,17 @@ export const NavbarLogo = () => {
       />
       <span className="font-medium text-black dark:text-white">
         {process.env.NEXT_PUBLIC_APP_NAME}
-      </span>
+      </span>*/}
+      <div className="mt-16 space-y-2">
+        <AppLogo />
+        {/*<Image
+          src="/logo.png"
+          width={160}
+          height={40}
+          className="object-contain"
+          alt="Appetiser Logo"
+        />*/}
+      </div>
     </a>
   );
 };
